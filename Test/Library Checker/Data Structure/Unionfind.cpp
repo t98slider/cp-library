f62e56d7c6bba0e443faf0f645b3a6629a55cb05
t98slider <72385984+t98slider@users.noreply.github.com>
2023-06-18 16:54:32 +0900

    Create Unionfind.cpp

---
+++ Test/Library Checker/Data Structure/Unionfind.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include <bits/stdc++.h>
+#include "../../../Graph/dsu.hpp"
+
+int main() {
+    std::cin.tie(0);
+    std::ios::sync_with_stdio(false);
+    int n, q;
+    std::cin >> n >> q;
+    dsu uf(n);
+    while(q--){
+        int cmd, u, v;
+        std::cin >> cmd >> u >> v;
+        if(cmd == 0) {
+            uf.merge(u, v);
+        } else {
+            std::cout << uf.same(u, v) << '\n';
+        }
+    }
+}
