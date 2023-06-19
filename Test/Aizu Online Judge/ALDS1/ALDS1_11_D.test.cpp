0d2ae7e1749d8f611bd72a2f17e26d43265d3e74
t98slider <72385984+t98slider@users.noreply.github.com>
2023-06-20 01:13:08 +0900

    Create ALDS1_11_D.test.cpp

---
+++ Test/Aizu Online Judge/ALDS1/ALDS1_11_D.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/5/ALDS1_11_D"
+
+#include <bits/stdc++.h>
+#include "../../../Graph/dsu.hpp"
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n, m, q, u, v;
+    cin >> n >> m;
+    dsu uf(n);
+    while(m--){
+        cin >> u >> v;
+        uf.merge(u, v);
+    }
+    cin >> q;
+    while(q--){
+        cin >> u >> v;
+        cout << (uf.same(u, v) ? "yes" : "no") << '\n';
+    }
+}
