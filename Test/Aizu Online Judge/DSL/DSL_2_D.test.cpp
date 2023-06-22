b7961495d645d6b375f3299bcedc1938d5eaa4e2
t98slider <72385984+t98slider@users.noreply.github.com>
2023-06-22 14:48:05 +0900

    Create DSL_2_D.test.cpp

---
+++ Test/Aizu Online Judge/DSL/DSL_2_D.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_D"
+
+#include <bits/stdc++.h>
+#include "../../../DataStructure/segtree_dual.hpp"
+
+using namespace std;
+int op(int af, int bf){ return af == -1 ? bf : af; }
+int id(){ return -1; }
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, q;
+    cin >> n >> q;
+    dual_segtree<int, op, id> seg(vector<int>(n, numeric_limits<int>::max()));
+    while(q--){
+        int cmd;
+        cin >> cmd;
+        if(cmd == 0){
+            int l, r, x;
+            cin >> l >> r >> x;
+            seg.apply(l, r + 1, x);
+        }else{
+            int p;
+            cin >> p;
+            cout << seg[p] << '\n';
+        }
+    }
+}
